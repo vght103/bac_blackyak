b2ec13f31051ed1916303669f21c7ac13341757e
vght103 <74849404+vght103@users.noreply.github.com>
2021-07-13 17:14:44 +0900

    project final

--- js/join.js
+++ js/join.js
@@ -1,20 +1,12 @@
-
-window.onload = function(){
-
+window.onload = function () {
   var policyBoxs = $(".instruction_box");
   policyBoxs.eq(0).css("display", "block");
-  
+
   var clickItems = $(".join_instruction > label > a");
   console.log(clickItems);
-  clickItems.click(function(e){
+  clickItems.click(function (e) {
     policyBoxs.css("display", "none");
     var currentIndex = e.target.className.replace("policy_", "");
     policyBoxs.eq(currentIndex - 1).css("display", "block");
   });
-}
-
-
-
-function joinComplete(){
-  confirm("회원가입을 완료하시겠습니까?");
 };
